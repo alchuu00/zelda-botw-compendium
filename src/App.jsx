04ce604b5bf71cc6595037b065047886868e7dab
Alja Čekada <alja.cekada@gmail.com>
2023-04-10 14:25:41 +0200

    add button to play music

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,12 @@ import TreasurePage from "./pages/TreasurePage";
 import ItemDisplay from "./pages/ItemDisplay";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import soundtrack from "./assets/main_theme_soundtrack.mp3"
 
 function App() {
   const [data, setData] = useState(null);
+  const [audio, setAudio] = useState(null);
+
   useEffect(() => {
     fetch("https://botw-compendium.herokuapp.com/api/v2/all")
       .then((resp) => resp.json())
@@ -20,22 +23,34 @@ function App() {
       });
   }, []);
 
-  console.log(data);
+  useEffect(() => {
+    const audioElement = new Audio(soundtrack);
+    audioElement.loop = true;
+    audioElement.muted = true;
+    setAudio(audioElement);
+  }, []);
+
+  const handlePlayButtonClick = () => {
+    if (audio) {
+      audio.play();
+      audio.muted = false;
+    }
+  };
 
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/creatures" element={<CreaturePage data={data}/>} />
-          <Route path="/monsters" element={<MonsterPage data={data}/>} />
-          <Route path="/equipment" element={<EquipmentPage data={data}/>} />
-          <Route path="/materials" element={<MaterialPage data={data}/>} />
-          <Route path="/treasure" element={<TreasurePage data={data}/>} />
-          <Route path="/item/:id" element={<ItemDisplay data={data}/>} />
+          <Route path="/" element={<Homepage handlePlayButtonClick={handlePlayButtonClick}/>} />
+          <Route path="/creatures" element={<CreaturePage data={data} />} />
+          <Route path="/monsters" element={<MonsterPage data={data} />} />
+          <Route path="/equipment" element={<EquipmentPage data={data} />} />
+          <Route path="/materials" element={<MaterialPage data={data} />} />
+          <Route path="/treasure" element={<TreasurePage data={data} />} />
+          <Route path="/item/:id" element={<ItemDisplay data={data} />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </div>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function Homepage() {
+function Homepage({ handlePlayButtonClick }) {
   return (
-    <div className='homepage'>
-    <img src='src\assets\The_Legend_of_Zelda_Breath_of_the_Wild_logo.png' className='homepage-img'></img>
+    <div className="homepage animation-appear">
+      <div className="homepage animation-appear">
+        <img
+          src="src\assets\The_Legend_of_Zelda_Breath_of_the_Wild_logo.png"
+          className="homepage-img"
+        ></img>
+        <p>For a better user experience click this button!</p>
+        <button onClick={handlePlayButtonClick}>Click Me</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
