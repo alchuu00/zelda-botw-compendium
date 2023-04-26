43066200d771845348de23959e91e54cf5e6d26a
Alja Čekada <alja.cekada@gmail.com>
2023-04-26 19:14:52 +0200

    add loading bar to homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-function Homepage() {
+function Homepage({ loading }) {
   return (
-    <div className="homepage animation-appear">
-      <div className="homepage animation-appear">
-        <img
+    <div className="homepage">
+      <img
           src="src\assets\The_Legend_of_Zelda_Breath_of_the_Wild_logo.png"
           className="homepage-img"
+          alt="Homepage Image"
         />
-      </div>
     </div>
   );
 }
